3172bfab9a46a1772e8ab3769fb4e62d4d0ef0c0
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-09-09 20:13:46 +0530

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const { data } = useFetch('/api/')
+</script>
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  serverHandlers: [
+    {
+      route: "/api",
+      handler: "~~/hono/index.ts",
+      middleware: true,
+    },
+  ],
+});
