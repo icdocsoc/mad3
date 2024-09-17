4b954168db19e430986cb9994753abb86e1d5664
Dropheart <ahmed@techfromtheheart.com>
2024-09-18 02:03:21 +0300

    remove unnecessary log + add 'authenticated'

--- app/middleware/restrictPage.ts
+++ app/middleware/restrictPage.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { user } = useAuth();
 
-  console.log('running')
-  if (user == undefined || user.value?.role != to.meta.auth) return abortNavigation();
+  if (user == undefined || (user.value?.role != to.meta.auth && to.meta.auth != 'authenticated')) return abortNavigation();
 });
