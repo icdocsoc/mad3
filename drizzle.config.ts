dce93f7ac0b5f236e214c8a93cab6e4d0a244b49
Dropheart <jay@abussaud.dev>
2024-09-21 02:15:32 +0300

    fix schema generation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './hono/admin/schema.ts',
+  schema: './hono/**/schema.ts',
   dialect: 'postgresql',
   verbose: true
 });
