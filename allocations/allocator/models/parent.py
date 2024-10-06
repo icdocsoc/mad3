b82db5a1a7d2068b835fd51f91743cfcb7482004
Dropheart <jay@abussaud.dev>
2024-10-06 19:11:34 +0100

    fixed allocator + clarity on auth cookie

--- allocations/allocator/models/parent.py
+++ allocations/allocator/models/parent.py
@@ -10,7 +10,6 @@ class Parent:
         self.student = Student(data['student'])
 
         self._interests = data['interests']
-        if data['interests'] == None: self._interests = {'alcohol': 1, 'anime': 0, 'artGraphics': 0, 'baking': 0, 'charity': 0, 'clubbing': 0, 'cooking': 0, 'danceBallroom': 1, 'danceContemporary': 0, 'dramatics': 1, 'exerciseAndHealth': 1, 'film': 1, 'finance': 2, 'football': 0, 'hiking': 0, 'kpop': 0, 'martialArts': 0, 'otherSports': 0, 'performingMusicClassical': 1, 'performingMusicPopRockJazz': 2, 'photography': 0, 'politics': 1, 'racketSports': 0, 'rowing': 0, 'rugby': 0, 'tabletopGames': 1, 'videoGames': 0}
 
         self.family = data['family']
 
